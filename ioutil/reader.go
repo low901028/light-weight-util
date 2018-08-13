93ec5338fa0335763124830e2776c2bbe16b7ad9
jdd <dalan_123@126.com>
2018-08-13 18:32:31 +0800

    新增reader和closer

---
+++ ioutil/reader.go
@@ -0,0 +1,25 @@
+package ioutil
+
+import "io"
+
+// 工具类
+func NewLimitedBufferReader(r io.Reader, n int) io.Reader{
+	return &limitedBufferReader{
+		r: r,
+		n: n,
+	}
+}
+
+// 实现Reader接口
+type limitedBufferReader struct {
+	r io.Reader
+	n int
+}
+
+func (r *limitedBufferReader) Read(p []byte) (n int, err error){
+	np := p
+	if len(np) > r.n{
+		np = np[:r.n]
+	}
+	return r.r.Read(np)
+}
